00b783d9ccd5731243d270d83d07ae9ba39331e7
6304673J8 <petergeorghe.julca@enti.cat>
2021-06-14 00:23:59 +0200

    Animations Working

--- pub/spaceships.js
+++ pub/spaceships.js
@@ -18,8 +18,9 @@ let enemy_speed;
 
 let bullets = [];
 let bullet_speed;
-
+let bullet_instantiate_x;
 let shooting = true;
+let explosion_sound;
 let score = 0;
 
 let up_key;
@@ -28,7 +29,7 @@ let space_key;
 
 const BULLET_INIT_X = 840;
 const BULLET_INIT_Y = 840;
-const MAX_ENEMIES = 16;
+const MAX_ENEMIES = 12;
 const MAX_BULLETS = 3;
 const COOLDOWN = 1;
 
@@ -37,39 +38,69 @@ const SCREEN_MARGIN = 32;
 function preload () {
 	console.log("Preload Status: OK");
 	this.load.image("bg", "Assets/BG/stars_bg.jpg");
-	this.load.image("ship", "Assets/Ships/Goon_Ship.png");
-	this.load.image("enemy", "Assets/Ships/Noog_Ship.png");
-	this.load.image("bullet", "Assets/Projectiles/Goon_Missile.png");
+	this.load.image("goon_fx", "Assets/FX/FX_GoonPlosion_Cruz.png");
+	this.load.audio("explosion_fx", "Assets/FX/Phaser_Explosion.ogg")
+	this.load.spritesheet("ship", "Assets/Spritesheets/Ships/Goon_Ship_SpriteSheet.png",
+			{frameWidth:75, frameHeight:96});
+	
+	this.load.spritesheet("enemy", "Assets/Spritesheets/Ships/Noog_Ship_SpriteSheet.png",
+			{frameWidth:84, frameHeight:78});
+	this.load.spritesheet("bullet", "Assets/Spritesheets/Projectiles/Goon_Missile_Spritesheet.png",
+			{frameWidth:30, frameHeight:14});
 }
 
 function create () {
 	enemies = [];
 	bullets = [];
 	game_screen = this;
+
+	ship_speed = 20;
+	bullet_speed = 10;
+	bullet_instantiate_x = 55;
+	enemy_speed = 4;
+
 	bgGraphs = this.make.graphics({ x: 0, y: 0, add: false });
 	bgGraphs.generateTexture('bg', 630, 370);
 	bgTileSprite = this.add.tileSprite(400, 300, 800, 600, 'bg');
+	
+	explosion_sound = this.sound.add("explosion_fx");
+	explosion_sound.setVolume(1);
+	//Animations
+	const shipAnimation = this.anims.create({
+		key: 'fly',
+		frames: this.anims.generateFrameNumbers('ship'),
+		frameRate:12
+	});
+	ship = this.physics.add.sprite(player_init_x, scene_h/2, "ship");
+	
+	const bulletAnimation = this.anims.create({
+		key: 'shot',
+		frames: this.anims.generateFrameNumbers('bullet'),
+		frameRate:12
+	});
 
-	ship = this.physics.add.image(player_init_x, scene_h/2, "ship");
-	//ship.setScale(1);
-	ship_speed = 20;
-	enemy_speed = 4;
-	bullet_speed = 10;
+	const enemyAnimation = this.anims.create({
+		key:'enemyShip',
+		frames: this.anims.generateFrameNumbers('enemy'),
+		frameRate:12
+	});
+	
 	scoreText = this.add.text(player_init_x, 24, 'Score: ' + score, {fontSize: '42px', fill: '#FFF' } );
 
 	for (let i = 0; i < MAX_ENEMIES; i++){
 		let x = Math.random() * scene_w * 4 + scene_w/2;
 		let y = Math.random() * scene_h;
 		
-		enemies.push(this.physics.add.image(x, y, "enemy"));
+		enemies.push(this.physics.add.sprite(x, y, "enemy"));
+		enemies[i].play('enemy_fly', true);
 
 		this.physics.add.collider(enemies[i]);
 	}
 
 	for (let i = 0; i < MAX_BULLETS; i++){
-		bullets.push(this.physics.add.image(BULLET_INIT_X, BULLET_INIT_Y, "bullet"));
+		bullets.push(this.physics.add.sprite(BULLET_INIT_X, BULLET_INIT_Y, "bullet"));
 		bullets[i].moving = false;
-
+		bullets[i].play('shot', true);
 		this.physics.add.collider(bullets[i]);
 	}
 
@@ -85,7 +116,9 @@ function create () {
 				bulletReset(element);
 				calculateScore();
 				particles.emitParticleAt(en.x, en.y);
-				en.destroy();
+				enemyReset(en);
+				explosion_sound.play();
+				//en.destroy();
 		},null,this);
 	});
 	
@@ -93,15 +126,15 @@ function create () {
 	down_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 	space_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 	
-	particles = this.add.particles('ship');
+	particles = this.add.particles('goon_fx');
 	particles.createEmitter({
 		angle: { min: 220, max: 300 },
 		speed: { min: 420, max: 560 },
 		quantity: { min: 2, max: 10 },
 		lifespan: 4000,
 		alpha: { start: 1, end: 0 },
-		scale: { min: 0.05, max: 0.4 },
-		rotate: { start: 0, end: 360, ease: 'Back.easeOut' },
+		scale: { min: 0.4, max: 1 },
+		rotate: { start: 0, end: 180, ease: 'Back.easeOut' },
 		gravityY: 720,
 		quantity: 32,
 		on: false
@@ -110,11 +143,20 @@ function create () {
 
 function update () {
 	bgTileSprite.TilePositionX += 1;
-
+	
+	for(let i = 0; i < MAX_BULLETS; i++){
+		bullets[i].play('shot', true);	
+	}
+	
+	//for ( let i = 0; i < MAX_ENEMIES; i++){
+	
+	//}
 	if(up_key.isDown){
+		ship.play('fly', true);
 		ship.y-- * ship_speed;
 	}
 	else if(down_key.isDown){
+		ship.play('fly', true);
 		ship.y++ * ship_speed;
 	}
 	
@@ -127,9 +169,9 @@ function update () {
 		shooting = false;
 		for(let i = 0; i < MAX_BULLETS && !found; i++){
 			if(!bullets[i].moving){
-				bullets[i].moving = true;
-				bullets[i].x = ship.x;
+				bullets[i].x = ship.x + bullet_instantiate_x;
 				bullets[i].y = ship.y;
+				bullets[i].moving = true;
 				found = true;
 			}
 		}
@@ -141,13 +183,13 @@ function update () {
 			if (bullets[i].x >= scene_w + SCREEN_MARGIN){
 				bullets[i].x = BULLET_INIT_X;
 				bullets[i].y = BULLET_INIT_Y;
-
-				bullets[i].moving = false;
+				bullets[i].moving = false;				
 			}
 		}
 	}
 
 	for ( let i = 0; i < MAX_ENEMIES; i++){
+		enemies[i].play('enemyShip', true);	
 		enemies[i].x-- * enemy_speed;
 	}
 }
@@ -156,6 +198,11 @@ function bulletReset(bullet){
 	bullet.x = BULLET_INIT_X;
 	bullet.y = BULLET_INIT_Y;
 	bullet.moving = false;
+}
+
+function enemyReset(enemy){
+	enemy.x = Math.random() * scene_w * 8 + scene_w/2;
+	enemy.y = Math.random() * scene_h;
 }
 
 function calculateScore(){
